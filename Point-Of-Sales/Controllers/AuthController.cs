5a7152c0dafe26be2c7371a93967ac6f500a483e
LCT1808 <lecamtu.0308@gmail.com>
2023-12-17 23:24:16 +0700

    login update

--- Point-Of-Sales/Controllers/AuthController.cs
+++ Point-Of-Sales/Controllers/AuthController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Point_Of_Sales.Config;
 
 namespace Point_Of_Sales.Controllers
 {
     public class AuthController : Controller
     {
+        private ApplicationDbContext context;
         public IActionResult Index()
         {
-            return View();
+            return View("Login");
         }
+
+        public async Task<IActionResult> Index(string username, string password)
+        {
+            var _account = context.Accounts.FirstOrDefault(a => a.Username.Equals(username) && a.Pwd.Equals(password));
+
+            if (_account == null)
+            {
+                ViewBag.Message = "Please check your credentials.";
+                return View("Login");
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+
     }
 }
